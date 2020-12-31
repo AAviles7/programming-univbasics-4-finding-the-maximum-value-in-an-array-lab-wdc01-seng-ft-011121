4b825009620fbb37ea2eea8ca9d6246c3e522179
Abraham Aviles <aaaviles1995@gmail.com>
2020-12-31 06:20:29 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,13 @@
 def find_max_value(array)
   # Add your solution here
+  
+  x = 0
+  ans = array[0]
+  array.legth.times do 
+    if ans < array[x]
+      ans = array[x]
+    end
+    x += 1
+  end
+  
 end
